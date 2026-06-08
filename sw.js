9d865c5e41a814735cb80ed37680a1ce06ee27ae
princemcuwuigiaren-hub <princemcuwuigiaren@gmail.com>
2026-06-08 22:23:44 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE = 'ja-rccg-v2';
+const APP_SHELL = [
+  './jerusalem-arena-rccg.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;0,700;1,400&family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => Promise.allSettled(APP_SHELL.map(url => c.add(url).catch(() => {}))))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  const isShell = url.pathname.match(/\.(html|json|png|woff2?)$/)
+    || url.hostname === 'fonts.googleapis.com'
+    || url.hostname === 'fonts.gstatic.com';
+
+  if (isShell) {
+    e.respondWith(
+      caches.match(e.request).then(cached => {
+        if (cached) return cached;
+        return fetch(e.request).then(res => {
+          if (res && res.status === 200) {
+            caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+          }
+          return res;
+        }).catch(() => cached);
+      })
+    );
+  } else {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          if (res && res.status === 200) {
+            caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+          }
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+  }
+});
